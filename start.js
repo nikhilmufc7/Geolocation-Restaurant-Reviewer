ff0aeb76f6cdd20970a6ade1554bc111d5da4850
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-03-25 20:52:29 +0530

    :fire:

--- start.js
+++ start.js
@@ -20,6 +20,11 @@ mongoose.connection.on('error', (err) => {
 // READY?! Let's go!
 
 
+// import models
+
+require('./models/Store');
+
+
 // Start our app!
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
